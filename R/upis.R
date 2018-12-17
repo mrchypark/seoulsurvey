520b4e26572a31a0e935963970b804efb75f9769
Chanyub Park <mrchypark@gmail.com>
2018-12-18 00:54:42 +0900

    add upis 2005 init

---
+++ R/upis.R
@@ -0,0 +1,9 @@
+#' Seoul Survey Urban Policy Index Survey
+#'
+#' now 2005 year data only
+#'
+#' @source http://data.si.re.kr/node/523
+#' @format Tibble with columns
+#' @examples
+#'   upis
+"upis"
